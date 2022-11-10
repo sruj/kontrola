501e976cf9a42798e21120c2aedc64db5f56cda0
pawel.chryplewicz <pawel.chryplewicz@wfirma.pl>
2022-11-10 13:06:37 +0100

    Baza, encja Product i Repozytorium przykładowe

---
+++ src/Controller/ProductController.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Product;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+use App\Repository\ProductRepository;
+
+class ProductController extends AbstractController
+{
+    /**
+     * @Route("/product_index", name="app_product")
+     */
+    public function index(): Response
+    {
+        return $this->render('product/index.html.twig', [
+            'controller_name' => 'ProductController',
+        ]);
+    }
+
+    /**
+     * @Route("/product", name="create_product")
+     */
+    public function createProduct(ManagerRegistry $doctrine, ValidatorInterface $validator): Response
+    {
+        $entityManager = $doctrine->getManager();
+
+        $product = new Product();
+        $product->setName('Keyboard');
+
+        $errors = $validator->validate($product);
+
+        if (count($errors) > 0) {
+            return new Response((string) $errors, 400);
+        }
+
+        // tell Doctrine you want to (eventually) save the Product (no queries yet)
+        $entityManager->persist($product);
+
+        // actually executes the queries (i.e. the INSERT query)
+        $entityManager->flush();
+
+        return new Response('Saved new product with id '.$product->getId());
+    }
+
+    /**
+     * @Route("/product/{id}", name="product_show")
+     */
+    public function show(ProductRepository $repository, ManagerRegistry $doctrine, int $id): Response
+    {
+        $product = $repository
+            ->find($id);
+        $product = $doctrine->getRepository(Product::class)->find($id);
+//        $product = $repository->findOneBy(['name' => 'Keyboard']);
+//        $products = $repository->findAll();
+
+        if (!$product) {
+            throw $this->createNotFoundException(
+                'No product found for id '.$id
+            );
+        }
+
+        return new Response('Check out this great product: '.$product->getName());
+    }
+
+    /**
+     * @Route("/product/edit/{id}", name="product_edit")
+     */
+    public function update(ManagerRegistry $doctrine, int $id): Response
+    {
+        $entityManager = $doctrine->getManager();
+        $product = $entityManager->getRepository(Product::class)->find($id);
+
+        if (!$product) {
+            throw $this->createNotFoundException(
+                'No product found for id '.$id
+            );
+        }
+
+        $product->setName('New product name!');
+        $entityManager->flush();
+
+        return $this->redirectToRoute('product_show', [
+            'id' => $product->getId()
+        ]);
+    }
+
+    /**
+     * @Route("/product/del/{id}", name="product_del")
+     */
+    public function del(ManagerRegistry $doctrine, int $id): Response
+    {
+        $entityManager = $doctrine->getManager();
+        $product = $entityManager->getRepository(Product::class)->find($id);
+
+        if (!$product) {
+            throw $this->createNotFoundException(
+                'No product found for id '.$id
+            );
+        }
+
+        $entityManager->remove($product);
+        $entityManager->flush();
+
+        return new Response('Usunięto: '.$product->getName());
+    }
+}
